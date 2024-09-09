4abb6f0fce36db8c5c49e4898cc641ca74acc591
Mobeen Ejaz <mobeene98@gmail.com>
2024-09-09 14:52:50 +0500

    team card in progress

--- src/components/teams/Teams.jsx
+++ src/components/teams/Teams.jsx
@@ -1,5 +1,6 @@
 import Heading from "../common/Heading";
 import "./teams.css";
+import avatar from "../../assets/teams/300-1.jpg";
 
 const Teams = () => {
   return (
@@ -11,25 +12,9 @@ const Teams = () => {
       <div className="teams__content">
         <div className="flower">
           <div className="flower__head">
-            <div className="flower__eye left">
-              <span></span>
+            <div className="flower__img_container">
+              <img src={avatar} alt="image" className="flower__img" />
             </div>
-            <div className="flower__nose"></div>
-            <div className="flower__eye right">
-              <span></span>
-            </div>
-          </div>
-
-          <div className="flower__leaf"></div>
-          <div className="flower__leaf--shadow"></div>
-          <div className="flower__leaf oposite"></div>
-          <div className="flower__leaf--shadow oposite"></div>
-
-          <div className="flower__stem"></div>
-
-          <div className="flower__pot">
-            <div className="flower__pot--shadow"></div>
-            <div className="flower__pot--bottom"></div>
           </div>
         </div>
       </div>
